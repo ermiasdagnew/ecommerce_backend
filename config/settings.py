bed6615fa2df97d550fbf4f50cf7202c99300e3e
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-24 09:53:56 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 DEBUG = True
-ALLOWED_HOSTS = ['ecommerce-backend-r8jm.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
